674de6313622a98dc8d01468987049e6540ef0ea
Alexandervt8 <145701447+Alexandervt8@users.noreply.github.com>
2024-07-21 19:54:53 -0500

    version 1

--- p_django/backend/urls.py
+++ p_django/backend/urls.py
@@ -1,1 +1,10 @@
+# backend/urls.py
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),  # Cambiado para servir el index.html de React
+]
 
